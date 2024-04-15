0d899c6ba718a7e146a305ae088c016dbd61bc04
saci zakaria <sacideveloppeur@gmail.com>
2024-04-15 14:55:16 +0200

    public TextMeshProUGUI text;

--- Assets/Script/Obstacle.cs
+++ Assets/Script/Obstacle.cs
@@ -8,6 +8,8 @@ public class Obstacle : MonoBehaviour
 {
     public float speed =10.0f;
 
+    public TextMeshProUGUI text;
+
 
     // Start is called before the first frame update
     void Start()
